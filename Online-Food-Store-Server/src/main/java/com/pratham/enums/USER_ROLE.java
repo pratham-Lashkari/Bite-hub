9a4655dcfbfcfaeaca561a69289ccf010a6fb074
Pratham <prathamlashkari841@gmail.com>
2024-07-09 14:58:11 +0530

    added enums for user

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/enums/USER_ROLE.java
@@ -0,0 +1,5 @@
+package com.pratham.enums;
+
+public enum USER_ROLE {
+
+}
--- Online-Food-Store-Server/src/main/java/com/pratham/model/User.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/User.java
@@ -1,8 +1,19 @@
 package com.pratham.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.pratham.enums.USER_ROLE;
+
 import lombok.Data;
 
 @Data
+@Document(collection = "User")
 public class User {
 
+  @Id
+  private String Id;
+  private String fullName;
+  private String email;
+  private String password;
+  private USER_ROLE role;
 }
