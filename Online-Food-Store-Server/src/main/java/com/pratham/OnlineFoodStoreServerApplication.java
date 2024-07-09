c48a718a586e1b75365627094a222420fff7da65
Pratham <prathamlashkari841@gmail.com>
2024-07-09 10:27:46 +0530

    setup basis server

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/OnlineFoodStoreServerApplication.java
@@ -0,0 +1,13 @@
+package com.pratham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodStoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodStoreServerApplication.class, args);
+	}
+
+}
