2429275847865e4379882e160ba31b16b0ceec40
Pratham <prathamlashkari841@gmail.com>
2024-07-09 17:21:33 +0530

    added restaurant dto in user

--- Online-Food-Store-Server/src/main/java/com/pratham/Dto/RestaurantDto.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/Dto/RestaurantDto.java
@@ -1,5 +1,13 @@
 package com.pratham.Dto;
 
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class RestaurantDto {
 
+  private String Id;
+  private String title;
+  private List<String> description;
 }
