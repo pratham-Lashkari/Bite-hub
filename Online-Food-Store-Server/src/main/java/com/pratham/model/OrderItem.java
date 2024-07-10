1cce7f10c6a84c9444ef1a43acf876c686d4197a
Pratham <prathamlashkari841@gmail.com>
2024-07-10 13:21:31 +0530

    created order items moedl comeplted

--- Online-Food-Store-Server/src/main/java/com/pratham/model/OrderItem.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/OrderItem.java
@@ -1,5 +1,7 @@
 package com.pratham.model;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -8,4 +10,9 @@ import lombok.Data;
 @Document(collection = "OrderItems")
 public class OrderItem {
 
+  private String id;
+  private Food food;
+  private int quantity;
+  private Long totalPrice;
+  private List<String> ingredients;
 }
