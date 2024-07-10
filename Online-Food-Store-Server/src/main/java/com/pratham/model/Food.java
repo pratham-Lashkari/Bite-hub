a7b843a886dea170094b54c3d3dbd861b16d1486
Pratham <prathamlashkari841@gmail.com>
2024-07-10 13:18:52 +0530

    added food model

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/Food.java
@@ -0,0 +1,13 @@
+package com.pratham.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Food {
+
+  private String id;
+}
