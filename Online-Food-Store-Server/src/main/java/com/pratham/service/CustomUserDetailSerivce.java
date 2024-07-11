5ff63f94d160783539b8fe91630a3fb76755f2a0
Pratham <prathamlashkari841@gmail.com>
2024-07-11 19:35:04 +0530

    removed unnecessary imports

--- Online-Food-Store-Server/src/main/java/com/pratham/service/CustomUserDetailSerivce.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/CustomUserDetailSerivce.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
