49e945625b2462d9938645fe002b9ce45cb33850
Pratham <prathamlashkari841@gmail.com>
2024-07-11 19:34:19 +0530

    added custom user detailsservice method

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/CustomUserDetailSerivce.java
@@ -0,0 +1,41 @@
+package com.pratham.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.pratham.enums.USER_ROLE;
+import com.pratham.model.User;
+import com.pratham.repository.UserRepository;
+
+@Service
+public class CustomUserDetailSerivce implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email);
+
+    if (user == null) {
+      throw new UsernameNotFoundException("User not found");
+    }
+    USER_ROLE role = user.getRole();
+    if (role == null)
+      role = USER_ROLE.ROLE_CUSTOMER;
+
+    List<GrantedAuthority> authorities = new ArrayList<>();
+    authorities.add(new SimpleGrantedAuthority(role.toString()));
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+  }
+
+}
