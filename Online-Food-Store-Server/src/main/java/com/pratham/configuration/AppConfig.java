8e0929ea464be65c88c39c1d73ad58a0615a5796
Pratham <prathamlashkari841@gmail.com>
2024-07-11 14:23:12 +0530

    adding corse configuration

--- Online-Food-Store-Server/src/main/java/com/pratham/configuration/AppConfig.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/configuration/AppConfig.java
@@ -5,6 +5,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
 
 @Configuration
 @EnableWebSecurity
@@ -19,8 +23,24 @@ public class AppConfig {
         .requestMatchers("/api/admin/**").hasAnyRole("RESTAURANT_OWNER", "ADMIN")
         .requestMatchers("/api/**").authenticated()
         .anyRequest().permitAll());
+    http.addFilterBefore(new JwtTokenValidatro(), BasicAuthenticationFilter.class);
+    http.csrf(csrf -> csrf.disable());
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
     return http.build();
+  }
+
+  private CorsConfigurationSource corsConfigurationSource() {
+    return new CorsConfigurationSource() {
+
+      @Override
+      public CorsConfiguration getCorsConfiguration(ServerWebExchange arg0) {
+
+        CorsConfiguration cfg = new CorsConfiguration();
+        return cfg;
+      }
+
+    };
   }
 
 }
