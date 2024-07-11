3c4e0b90daa17c8e692cb642ac2d9e03393b0000
Pratham <prathamlashkari841@gmail.com>
2024-07-11 20:12:41 +0530

    added cart repository

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.pratham.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.pratham.model.Cart;
+
+public interface CartRepository extends MongoRepository<Cart, String> {
+
+}
