4c683b77db796ab1449a5ce2258999435f1dcd43
Pratham <prathamlashkari841@gmail.com>
2024-07-11 20:40:09 +0530

    created login mehtods

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/AtuhController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/AtuhController.java
@@ -1,11 +1,15 @@
 package com.pratham.controller;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pratham.configuration.JwtProvider;
+import com.pratham.enums.USER_ROLE;
 import com.pratham.model.Cart;
 import com.pratham.model.User;
 import com.pratham.repository.CartRepository;
 import com.pratham.repository.UserRepository;
+import com.pratham.request.LoginRequest;
 import com.pratham.response.AuthResponse;
 import com.pratham.service.CustomUserDetailSerivce;
 
@@ -68,8 +74,35 @@ public class AtuhController {
     return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
   }
 
-  public ResponseEntity<AuthResponse> login() throws Exception {
+  @PostMapping("/login")
+  public ResponseEntity<AuthResponse> login(LoginRequest req) throws Exception {
 
+    String email = req.getEmail();
+    String password = req.getPassword();
+
+    Authentication authentication = authenticate(email, password);
+
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+    String jwt = jwtProvider.generateToken(authentication);
+
+    AuthResponse authResponse = new AuthResponse();
+    authResponse.setToken(jwt);
+    authResponse.setMessage("User created Successfully");
+    authResponse.setRole(USER_ROLE.valueOf(role));
+
+    return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+  }
+
+  private Authentication authenticate(String email, String password) throws Exception {
+    UserDetails userDetails = customUserDetailSerivce.loadUserByUsername(email);
+    if (userDetails == null) {
+      throw new Exception("Inavlid User email");
+    }
+    if (passwordEncoder.matches(password, userDetails.getPassword())) {
+      throw new Exception("Inavlid User password");
+    }
+    return new UsernamePasswordAuthenticationToken(email, null, userDetails.getAuthorities());
   }
 
 }
