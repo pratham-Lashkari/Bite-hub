ad8c768fb624c3289283073675f4f2ed4e9fbf1f
Pratham <prathamlashkari841@gmail.com>
2024-07-14 13:25:41 +0530

    modiied user model

--- Online-Food-Store-Server/src/main/java/com/pratham/model/User.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pratham.Dto.RestaurantDto;
 import com.pratham.enums.USER_ROLE;
 
@@ -23,6 +25,9 @@ public class User {
   private String id;
   private String fullName;
   private String email;
+
+  @JsonIgnore
+  @JsonProperty(value = "password")
   private String password;
   private USER_ROLE role;
 
