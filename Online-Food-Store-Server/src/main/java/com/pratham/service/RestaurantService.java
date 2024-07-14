6fdc2b3a5c6fc02bfb7537230fc56055ea48f57b
Pratham <prathamlashkari841@gmail.com>
2024-07-14 20:43:06 +0530

    adding createrestaurant method

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/RestaurantService.java
@@ -0,0 +1,12 @@
+package com.pratham.service;
+
+import com.pratham.model.Restaurant;
+import com.pratham.model.User;
+
+public interface RestaurantService {
+
+  public Restaurant createRestaurant(CreteRestauratnRequest req , User user)
+  {
+    
+  }
+}
