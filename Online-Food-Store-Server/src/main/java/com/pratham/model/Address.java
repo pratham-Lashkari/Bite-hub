dbd9588b953d80987516c2078571c82f10cbb902
Pratham <prathamlashkari841@gmail.com>
2024-07-16 17:16:50 +0530

    added address class model and now have to edit configuration for role based authentictations

--- Online-Food-Store-Server/src/main/java/com/pratham/model/Address.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/Address.java
@@ -1,5 +1,24 @@
 package com.pratham.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "Address")
 public class Address {
+
+  @Id
+  private String id;
+  private String streetAddress;
+  private String city;
+  private String stateProvince;
+  private String postalCode;
+  private String country;
 
 }
