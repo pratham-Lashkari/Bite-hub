5f31371fa658684341fea596c2852c8f7e81c94d
Pratham <prathamlashkari841@gmail.com>
2024-07-17 10:47:07 +0530

    adding methods in foodserice

--- Online-Food-Store-Server/src/main/java/com/pratham/request/CreateFoodRequest.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/request/CreateFoodRequest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import lombok.Data;
 
 @Data
-public class CreateFoorRequest {
+public class CreateFoodRequest {
 
   private String name;
   private String description;
--- Online-Food-Store-Server/src/main/java/com/pratham/service/FoodService.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/FoodService.java
@@ -1,5 +1,17 @@
 package com.pratham.service;
 
+import java.util.List;
+
+import com.pratham.model.Category;
+import com.pratham.model.Food;
+import com.pratham.model.Restaurant;
+import com.pratham.request.CreateFoodRequest;
+
 public interface FoodService {
 
+  public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant);
+
+  void deleteFood(String foodId) throws Exception;
+
+  public List<Food> getRestaurantFood(String restaurantId, boolean isVegitarain, boolean isSeasonal);
 }
