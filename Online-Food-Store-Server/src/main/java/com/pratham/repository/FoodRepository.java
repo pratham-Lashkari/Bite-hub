95213acaa0b8089704c01dd964da084e4d8a1a8d
Pratham <prathamlashkari841@gmail.com>
2024-07-17 11:30:01 +0530

    added food repository

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/repository/FoodRepository.java
@@ -0,0 +1,19 @@
+package com.pratham.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.pratham.model.Food;
+
+@Repository
+public interface FoodRepository extends MongoRepository<Food, String> {
+
+  List<Food> findByRestaurantId(String restaurantId);
+
+  @Query("{ $or: [ { 'name': { $regex: :#{#keyword}, $options: 'i' } }, { 'foodcategory.name': { $regex: :#{#keyword}, $options: 'i' } } ] }")
+  List<Food> searchFood(@Param("keyword") String keyword);
+}
