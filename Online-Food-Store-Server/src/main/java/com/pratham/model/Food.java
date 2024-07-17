52b1064e99fac511c97da87439be0c200bc1e502
Pratham <prathamlashkari841@gmail.com>
2024-07-17 14:04:20 +0530

    added methods of creating food

--- Online-Food-Store-Server/src/main/java/com/pratham/model/Food.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/model/Food.java
@@ -26,7 +26,7 @@ public class Food {
   private Category foodCategory;
   private List<String> images;
   private boolean available;
-  private Restaurant restaurant;
+  private String restaurant;
   private boolean isSeasonal;
   private boolean isVegetarian;
   private List<String> ingredients = new ArrayList<>();
--- Online-Food-Store-Server/src/main/java/com/pratham/service/impl/FoodServiceImpl.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/impl/FoodServiceImpl.java
@@ -2,20 +2,36 @@ package com.pratham.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pratham.model.Category;
 import com.pratham.model.Food;
 import com.pratham.model.Restaurant;
+import com.pratham.repository.FoodRepository;
 import com.pratham.request.CreateFoodRequest;
 import com.pratham.service.FoodService;
 
 @Service
 public class FoodServiceImpl implements FoodService {
 
+  @Autowired
+  private FoodRepository foodRepository;
+
   @Override
   public Food createFood(CreateFoodRequest req, Category category, Restaurant restaurant) {
-    return null;
+    Food food = new Food();
+    food.setFoodCategory(category);
+    food.setRestaurant(restaurant.getId());
+    food.setDescription(req.getDescription());
+    food.setImages(req.getImages());
+    food.setName(req.getName());
+    food.setPrice(req.getPrice());
+    food.setIngredients(req.getIngredientsItem());
+    food.setSeasonal(req.isSessional());
+
+    Food savedFood = foodRepository.save(food);
+    return savedFood;
   }
 
   @Override
@@ -27,25 +43,21 @@ public class FoodServiceImpl implements FoodService {
   @Override
   public List<Food> getRestaurantFood(String restaurantId, boolean isVegitarain, boolean isSeasonal,
       String foodCategory, boolean isNonveg) {
-    // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'getRestaurantFood'");
   }
 
   @Override
   public List<Food> searchFood(String keyword) {
-    // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'searchFood'");
   }
 
   @Override
   public Food findFoodById(String foodId) throws Exception {
-    // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'findFoodById'");
   }
 
   @Override
   public Food updateAvailibilityStatus(String foodId) throws Exception {
-    // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'updateAvailibilityStatus'");
   }
 
