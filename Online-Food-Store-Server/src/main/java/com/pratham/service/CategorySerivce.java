d372f0c495af216f1ecf5066d01c8b1995de31be
Pratham <prathamlashkari841@gmail.com>
2024-07-18 09:19:55 +0530

    Created category service interface now implementing its class

--- Online-Food-Store-Server/src/main/java/com/pratham/service/CategorySerivce.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/CategorySerivce.java
@@ -1,5 +1,14 @@
 package com.pratham.service;
 
+import java.util.List;
+
+import com.pratham.model.Category;
+
 public interface CategorySerivce {
 
+  public Category createcCategory(String name, String userId);
+
+  public List<Category> findCategoryRestaurantId(String id) throws Exception;
+
+  public Category findCategoryById(String id) throws Exception;
 }
---
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.pratham.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.pratham.model.Category;
+import com.pratham.service.CategorySerivce;
+
+@Service
+public class CategoryServiceImpl implements CategorySerivce {
+
+  @Override
+  public Category createcCategory(String name, String userId) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createcCategory'");
+  }
+
+  @Override
+  public List<Category> findCategoryRestaurantId(String id) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'findCategoryRestaurantId'");
+  }
+
+  @Override
+  public Category findCategoryById(String id) throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'findCategoryById'");
+  }
+
+}
