0b9d758b4d2b72d246140bc044d8f783b229175f
Pratham <prathamlashkari841@gmail.com>
2024-07-18 10:20:29 +0530

    creted ingridents serivce now creating its impl clas

--- Online-Food-Store-Server/src/main/java/com/pratham/service/IngredientCategory.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/IngredientCategory.java
@@ -1,5 +1,19 @@
 package com.pratham.service;
 
+import java.util.List;
+
+import com.pratham.model.IngredientsItem;
+
 public interface IngredientCategory {
 
+  public IngredientCategory createIngredientCategory(String name, String restaurantId) throws Exception;
+
+  public IngredientCategory findIngredientCategoryById(String id) throws Exception;
+
+  public List<IngredientCategory> findIngredientCategoryByRestaurantId(String id) throws Exception;
+
+  public IngredientsItem createIngredientItem(String restaurantId, String ingredientName, String categoryId)
+      throws Exception;
+
+  public List<IngredientsItem> findRestaurantIngredientes(String restaurantId);
 }
