3ce77d9b326d869487ac21023022ba9309628a16
Pratham <prathamlashkari841@gmail.com>
2024-07-19 21:36:42 +0530

    created admin controller for edit ingredients

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
@@ -0,0 +1,10 @@
+package com.pratham.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/ingredients")
+public class IngredientController {
+
+}
