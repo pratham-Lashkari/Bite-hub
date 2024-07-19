2f238b66331fc6595d1e25b77b67a1084a1bb5c0
Pratham <prathamlashkari841@gmail.com>
2024-07-19 21:42:49 +0530

    added dependecny in controller

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
@@ -1,10 +1,16 @@
 package com.pratham.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pratham.service.impl.IngredientServiceImpl;
 
 @RestController
 @RequestMapping("/api/admin/ingredients")
 public class IngredientController {
+
+  @Autowired
+  private IngredientServiceImpl ingredientServiceImpl;
 
 }
