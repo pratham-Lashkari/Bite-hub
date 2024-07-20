81134780e14777871bb9c9cfb990d1182ae4895f
Pratham <prathamlashkari841@gmail.com>
2024-07-20 14:25:24 +0530

    created methods for updating sotck

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
@@ -3,7 +3,9 @@ package com.pratham.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +37,13 @@ public class IngredientController {
     IngredientsItem item = ingredientServiceImpl.createIngredientItem(req.getRestaurantId(), req.getName(),
         req.getCategoryId());
     return new ResponseEntity<>(item, HttpStatus.CREATED);
-
   }
+
+  @PutMapping("/{id}/stock")
+  public ResponseEntity<IngredientsItem> updateIngredientStock(@PathVariable String id)
+      throws Exception {
+    IngredientsItem item = ingredientServiceImpl.updateStock(id);
+    return new ResponseEntity<>(item, HttpStatus.OK);
+  }
+
 }
