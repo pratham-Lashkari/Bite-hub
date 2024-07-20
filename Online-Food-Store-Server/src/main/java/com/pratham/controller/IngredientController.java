97aa304f23fea7993061090b0203ba37a513d6fb
Pratham <prathamlashkari841@gmail.com>
2024-07-20 14:00:19 +0530

    added ingredientCategroyRequest

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/IngredientController.java
@@ -1,9 +1,13 @@
 package com.pratham.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pratham.request.IngredientCategoryRequest;
+import com.pratham.service.IngredientCategory;
 import com.pratham.service.impl.IngredientServiceImpl;
 
 @RestController
@@ -13,4 +17,7 @@ public class IngredientController {
   @Autowired
   private IngredientServiceImpl ingredientServiceImpl;
 
+  public ResponseEntity<IngredientCategory> createIngredientCategory(@RequestBody IngredientCategoryRequest req) {
+    return null;
+  }
 }
---
+++ Online-Food-Store-Server/src/main/java/com/pratham/request/IngredientCategoryRequest.java
@@ -0,0 +1,9 @@
+package com.pratham.request;
+
+import lombok.Data;
+
+@Data
+public class IngredientCategoryRequest {
+  private String name;
+  private String restaurantId;
+}
