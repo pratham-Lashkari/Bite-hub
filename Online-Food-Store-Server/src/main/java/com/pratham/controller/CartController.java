75e6c585a59a9d6c73bd701a60c6e37796a319ac
Pratham <prathamlashkari841@gmail.com>
2024-07-20 18:06:41 +0530

    created controlle rfor remvoe cart

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/CartController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/CartController.java
@@ -3,6 +3,8 @@ package com.pratham.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pratham.model.Cart;
 import com.pratham.model.CartItem;
 import com.pratham.request.AddCartItemRequest;
+import com.pratham.request.UpdateCartItemRequest;
 import com.pratham.service.CartService;
 
 @RestController
@@ -29,9 +33,16 @@ public class CartController {
   }
 
   @PutMapping("/cart-item/update")
-  public ResponseEntity<CartItem> updateCartItemQuantity(@RequestBody AddCartItemRequest req,
+  public ResponseEntity<CartItem> updateCartItemQuantity(@RequestBody UpdateCartItemRequest req,
       @RequestHeader("Authorization") String jwt) throws Exception {
-    CartItem cartItem = cartService.addItemToCart(req, jwt);
+    CartItem cartItem = cartService.updateCartItemQuantity(req.getCartItemId(), req.getQuantity());
+    return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/cart-item/{id}/remove")
+  public ResponseEntity<Cart> removeCartItem(@PathVariable String id,
+      @RequestHeader("Authorization") String jwt) throws Exception {
+    Cart cartItem = cartService.removeItemFormCart(id, jwt);
     return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
   }
 
