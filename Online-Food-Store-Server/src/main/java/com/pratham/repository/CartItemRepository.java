75a8ac250a0edb54f213445396d058dd89c9852f
Pratham <prathamlashkari841@gmail.com>
2024-07-20 14:57:18 +0530

    added methods in cartitem repository

--- Online-Food-Store-Server/src/main/java/com/pratham/repository/CartItemRepository.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/repository/CartItemRepository.java
@@ -3,9 +3,11 @@ package com.pratham.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.pratham.model.Cart;
 import com.pratham.model.CartItem;
 
 @Repository
 public interface CartItemRepository extends MongoRepository<CartItem, String> {
 
+  public Cart findByCustomerId(String userId);
 }
