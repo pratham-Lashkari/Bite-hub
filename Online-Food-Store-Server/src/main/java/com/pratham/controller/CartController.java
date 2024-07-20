ac7b1e09ed8e6d803534d8068dc29ed21f0d41ca
Pratham <prathamlashkari841@gmail.com>
2024-07-20 17:48:45 +0530

    created caritem controller

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/CartController.java
@@ -0,0 +1,30 @@
+package com.pratham.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pratham.model.CartItem;
+import com.pratham.request.AddCartItemRequest;
+import com.pratham.service.CartService;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+  @Autowired
+  private CartService cartService;
+
+  @PutMapping("/cart/add")
+  public ResponseEntity<CartItem> addItemCart(@RequestBody AddCartItemRequest req,
+      @RequestHeader("Authorization") String jwt) throws Exception {
+    CartItem cartItem = cartService.addItemToCart(req, jwt);
+    return new ResponseEntity<>(cartItem, HttpStatus.CREATED);
+  }
+
+}
