f978b27a75701fa40a30c75bc4a00d974bc9f47f
Pratham <prathamlashkari841@gmail.com>
2024-07-21 11:15:03 +0530

    created orderserivce

---
+++ Online-Food-Store-Server/src/main/java/com/pratham/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.pratham.service;
+
+import java.util.List;
+
+import com.pratham.model.Order;
+import com.pratham.model.User;
+import com.pratham.request.OrderRequest;
+
+public interface OrderService {
+
+  public Order createOrder(OrderRequest order, User user);
+
+  public Order updateOrder(String orderId, String orderStatus) throws Exception;
+
+  public void cancelOrder(String orderId) throws Exception;
+
+  public List<Order> getUserOrder(String userId) throws Exception;
+
+  public List<Order> getRestaurantsOrder(String restaurantId, String orderStatus) throws Exception;
+}
