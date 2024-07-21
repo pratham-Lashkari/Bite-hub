dabcf62c48cb6f1f761e75ee025f6a77ac5f3615
Pratham <prathamlashkari841@gmail.com>
2024-07-21 11:06:04 +0530

    add findcartbyuserid

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/CartController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/CartController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -48,8 +49,14 @@ public class CartController {
 
   @PutMapping("/cart/clear")
   public ResponseEntity<Cart> clearCart(@RequestHeader("Authorization") String jwt) throws Exception {
-    Cart cartItem = cartService.clearCart(jwt);
-    return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    Cart cart = cartService.clearCart(jwt);
+    return new ResponseEntity<>(cart, HttpStatus.OK);
+  }
+
+  @GetMapping("/cart")
+  public ResponseEntity<Cart> findUserCart(@RequestHeader("Authorization") String jwt) throws Exception {
+    Cart cart = cartService.findCartByUserId(jwt);
+    return new ResponseEntity<>(cart, HttpStatus.OK);
   }
 
 }
