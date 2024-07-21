1f1c11a1a8ebac99cfab2b04cc586f3f1d5b04d1
Pratham <prathamlashkari841@gmail.com>
2024-07-21 18:48:09 +0530

    creating order controller

--- Online-Food-Store-Server/src/main/java/com/pratham/controller/OrderController.java
+++ Online-Food-Store-Server/src/main/java/com/pratham/controller/OrderController.java
@@ -1,8 +1,11 @@
 package com.pratham.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,5 +35,15 @@ public class OrderController {
     User user = userService.findUserByJwtToken(jwt);
     Order order = orderService.createOrder(req, user);
     return new ResponseEntity<>(order, HttpStatus.CREATED);
+  }
+
+  @GetMapping("/order/user")
+  public ResponseEntity<List<Order>> getOrderHistory(@RequestBody OrderRequest req,
+      @RequestHeader("Authorization") String jwt)
+      throws Exception {
+
+    User user = userService.findUserByJwtToken(jwt);
+    List<Order> orders = orderService.getUserOrder(user.getId());
+    return new ResponseEntity<>(orders, HttpStatus.OK);
   }
 }
