3f71097d72b9ba8043dacd89feacda92569442a8
Pratham <prathamlashkari841@gmail.com>
2024-07-22 10:59:53 +0530

    createing home page

---
+++ Online-Food-Store-Client/src/Component/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=''>
+      <section className='banner -z-50 relative flex flex-col justify-center items-center'> 
+        <div className='w-[50vw]'>
+
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
--- Online-Food-Store-Client/src/Component/NavBar.jsx
+++ Online-Food-Store-Client/src/Component/NavBar.jsx
@@ -1,40 +1,36 @@
-import React from 'react'
-import { Avatar, Badge, IconButton } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { pink } from '@mui/material/colors';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import React from "react";
+import { Avatar, Badge, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { pink } from "@mui/material/colors";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const NavBar = () => {
   return (
-    <div className='px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between'>
-
-        <div className='flex items-center space-x-4'>
-            <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
-                <li className='logo font-bold text-gray-300 text-2xl'>
-                    BiteHub
-                </li>
-            </div>
-
+    <div className="px-5 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
+      <div className="flex items-center space-x-4">
+        <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
+          <li className="logo font-bold text-gray-300 text-2xl">BiteHub</li>
         </div>
-            <div className='flex items-center space-x-2 lg:space-x-10'>
-                   <div className=''>
-                      <IconButton>
-                         <SearchIcon sx={{fontSize:"1.5rem"}}/>
-                      </IconButton>
-                   </div>
-                   <div className=''>
-                          <Avatar sx={{bgcolor:"white",color:pink.A400}}>C</Avatar>
-                   </div>
-                   <div className=''>
-                          <IconButton>
-                            <Badge color='secondary' badgeContent={3}>
-                              <ShoppingCartIcon sx={{fontSize:"1.5rem"}}></ShoppingCartIcon>
-                            </Badge>
-                          </IconButton>
-                   </div>
-            </div>
+      </div>
+      <div className="flex items-center space-x-2 lg:space-x-10">
+        <div className="">
+          <IconButton>
+            <SearchIcon sx={{ fontSize: "1.5rem" }} />
+          </IconButton>
+        </div>
+        <div className="">
+          <Avatar sx={{ bgcolor: "white", color: pink.A400 }}>C</Avatar>
+        </div>
+        <div className="">
+          <IconButton>
+            <Badge color="secondary" badgeContent={3}>
+              <ShoppingCartIcon sx={{ fontSize: "1.5rem" }}></ShoppingCartIcon>
+            </Badge>
+          </IconButton>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
