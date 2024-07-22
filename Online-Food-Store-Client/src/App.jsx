3d040ba503327cf8eed05dc69cac0fab40bd840b
Pratham <prathamlashkari841@gmail.com>
2024-07-22 11:16:19 +0530

    integrated background image correctly

--- Online-Food-Store-Client/src/App.jsx
+++ Online-Food-Store-Client/src/App.jsx
@@ -4,6 +4,7 @@ import { darkTheme } from './Theme/DarkTheme'
 import NavBar from './Component/NavBar'
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
+import Home from './Component/Home';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <ThemeProvider theme={darkTheme}>
           <CssBaseline/>
             <NavBar/>
+            <Home/>
         </ThemeProvider>
   )
 }
--- Online-Food-Store-Client/src/Component/Home.jsx
+++ Online-Food-Store-Client/src/Component/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import '../styles/Home.scss'
 const Home = () => {
   return (
-    <div className=''>
-      <section className='banner -z-50 relative flex flex-col justify-center items-center'> 
-        <div className='w-[50vw]'>
+    <div className="">
+      <section className="banner -z-50 relative flex flex-col justify-center items-center">
+        <div className="w-[50vw] z-10 text-center">
+          <p className="text-2xl lg:text-6xl font-bold z-10 py-5">Bite Hub</p>
+          <p className="z-10 text-gray-300 text-xl lg:text-4xl">
+            Taste the Convenience Food, Fast and Delivered.
+          </p>
+        </div>
+        <div className="cover absolute top-0 left-0 ring-0">
 
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
