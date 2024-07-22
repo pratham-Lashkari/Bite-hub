24cb85ab0397b0b584ecbb9f8c922d8731b1f979
Pratham <prathamlashkari841@gmail.com>
2024-07-22 13:11:13 +0530

    added constant meels

---
+++ Online-Food-Store-Client/src/constants/topMeel.js
@@ -0,0 +1,30 @@
+export const topMeals = [
+  {
+    image: "https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg",
+    title: "Burger",
+  },
+  {
+    image: "https://miro.medium.com/v2/resize:fit:1400/0*oTfm1pTXLxitHHFy.jpg",
+    title: "Pizza",
+  },
+  {
+    image: "https://cdn.sanity.io/images/263h0ltd/production/ead554e52dce234c63a4470e672036cf9c447a2a-1200x1000.webp",
+    title: "Sushi",
+  },
+  {
+    image: "https://cdn.loveandlemons.com/wp-content/uploads/2021/04/green-salad.jpg",
+    title: "Salad",
+  },
+  {
+    image: "https://assets.bonappetit.com/photos/64934404ae1041da97d81470/1:1/w_2560%2Cc_limit/20230524-SEO-RECIPES-BON-APP24118.jpg",
+    title: "Spaghetti",
+  },
+  {
+    image: "https://www.inspiredtaste.net/wp-content/uploads/2024/03/Easy-Fluffy-Pancakes-Recipe-Video.jpg",
+    title: "Pancakes",
+  },
+  {
+    image: "https://img.taste.com.au/w_-0kcUJ/taste/2016/11/aussie-style-beef-and-salad-tacos-86525-1.jpeg",
+    title: "Taco",
+  }
+];
