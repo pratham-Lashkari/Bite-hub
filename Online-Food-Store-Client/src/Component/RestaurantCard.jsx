0dba35e0afab3951ab0c93d100593797b05be7ad
Pratham <prathamlashkari841@gmail.com>
2024-07-23 17:39:23 +0530

    creating restaurant card

--- Online-Food-Store-Client/src/Component/RestaurantCard.jsx
+++ Online-Food-Store-Client/src/Component/RestaurantCard.jsx
@@ -1,10 +1,29 @@
+import {Card, Chip} from '@mui/material'
 import React from 'react'
+
 
 const RestaurantCard = () => {
   return (
-    <div>
-      
-    </div>
+    <Card>
+        <div className={`${true ? 'cursor-pointer' : 'cursor-not-allowed'} relative`}>
+            <img className='w-full h-[10rem] rounded-t-md object-cover' 
+             src="https://t3.ftcdn.net/jpg/03/24/73/92/360_F_324739203_keeq8udvv0P2h1MLYJ0GLSlTBagoXS48.jpg" alt="" />
+
+             <Chip 
+              size='small'
+              className='absolute top-2 left-2'
+              color={true?"success":"error"}
+              label={true ? "open" :"closed"} >
+             </Chip>
+        </div>
+        <div className='p-4 textPart lg:flex w-full justify-between'>
+            <div className='space-y-1'>
+                <p className='text-gray-500 text-sm'>
+                    Craving it all? Dive into our global fla...
+                </p>
+            </div>
+        </div>
+    </Card>
   )
 }
 
