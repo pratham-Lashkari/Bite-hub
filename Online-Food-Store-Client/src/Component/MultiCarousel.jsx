918385493bbe37fd0ffbf68a939f7cdaa24bd32e
Pratham <prathamlashkari841@gmail.com>
2024-07-23 14:19:24 +0530

    creating carousel item componenet

--- Online-Food-Store-Client/src/Component/MultiCarousel.jsx
+++ Online-Food-Store-Client/src/Component/MultiCarousel.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-Slick';
+
 
 const MultiCarousel = () => {
   return (
-    <div>MultiCarousel</div>
+    <div>
+      <Slider>
+      </Slider>
+    </div>
   )
 }
 
