4e1e86de37df46b679d7f994cd98fb83cb7f183c
Pratham <prathamlashkari841@gmail.com>
2024-07-23 14:25:20 +0530

    integrated coursoels

--- Online-Food-Store-Client/src/Component/MultiCarousel.jsx
+++ Online-Food-Store-Client/src/Component/MultiCarousel.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-Slick';
+import { topMeals } from '../constants/topMeel';
+import CarouselItem from './CarouselItem';
 
 
 const MultiCarousel = () => {
   return (
     <div>
       <Slider>
+        {
+          topMeals.map((item , ind)=>{
+            return <CarouselItem 
+            image={item.image}
+            title={item.title}
+            key={ind} />
+          })
+        }
       </Slider>
     </div>
   )
