bfffa9d88c5c9e2c613a7ef8276fc62cd1433419
Pratham <prathamlashkari841@gmail.com>
2024-07-25 10:52:31 +0530

    creating add to cart funtion

--- Online-Food-Store-Client/src/Component/Cart/CartItem.jsx
+++ Online-Food-Store-Client/src/Component/Cart/CartItem.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const CartItem = () => {
   return (
-    <div>
-      CartItems
+    <div className="px-5">
+      <div className="lg:flex items-center lg:space-x-5">
+        <div>
+          <img
+            className="w-[5rem] h-[5rem] object-cover"
+            src="https://miro.medium.com/v2/resize:fit:1400/0*oTfm1pTXLxitHHFy.jpg"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-between lg:w-[70%]">
+          <div className="space-y-1 lg:space-y-3 w-full">
+            <p>Pizza</p>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-1"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartItem
+export default CartItem;
