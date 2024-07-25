236902e58d6aff7fd13955ee2ac0ecd1ce57ee72
Pratham <prathamlashkari841@gmail.com>
2024-07-25 16:51:46 +0530

    created address page

--- Online-Food-Store-Client/src/Component/Cart/Cart.jsx
+++ Online-Food-Store-Client/src/Component/Cart/Cart.jsx
@@ -1,14 +1,47 @@
-import { Button, Card, Divider } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Modal,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
 import CartItem from "./CartItem";
 import AddressCard from "./AddressCard";
 import AddLocationIcon from "@mui/icons-material/AddLocation";
+import { ErrorMessage, Field, Formik, Form } from "formik";
 
 const Cart = () => {
+  const [open, setopen] = useState(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const initialValues = {
+    streetAddress: "",
+    state: "",
+    pincode: "",
+    city: "",
+  };
+
   const handleSelectAddress = () => {};
-  const handleOpenAddressModal = () => {};
+  const handleOpenAddressModal = () => setopen(true);
+  const handleClose = () => setopen(false);
+  const handleSubmit = (values) => console.log(values);
 
   return (
+    <>
       <main className="lg:flex justify-between">
         <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
           {[1, 2].map((ind) => (
@@ -45,8 +78,9 @@ const Cart = () => {
               Choose Delivery Address
             </h1>
             <div className="flex gap-5 flex-wrap justify-center">
-              {[1, 2, 3].map((item) => (
+              {[1, 2, 3].map((item,ind) => (
                 <AddressCard
+                  key={ind}
                   item={item}
                   showButton={true}
                   handleSelectAddress={handleSelectAddress}
@@ -72,6 +106,93 @@ const Cart = () => {
           </div>
         </section>
       </main>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="Street Address"
+                    name="streetAddress"
+                    fullWidth
+                    variant="outlined"
+                    error={!ErrorMessage("Street Address")}
+                    helprText={
+                      <ErrorMessage>
+                        {(msg) => <span className="text-red-600">{msg}</span>}
+                      </ErrorMessage>
+                    }
+                  ></Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="State"
+                    name="state"
+                    fullWidth
+                    variant="outlined"
+                    error={!ErrorMessage("State")}
+                    helprText={
+                      <ErrorMessage>
+                        {(msg) => <span className="text-red-600">{msg}</span>}
+                      </ErrorMessage>
+                    }
+                  ></Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="City"
+                    name="city"
+                    fullWidth
+                    variant="outlined"
+                    error={!ErrorMessage("City")}
+                    helprText={
+                      <ErrorMessage>
+                        {(msg) => <span className="text-red-600">{msg}</span>}
+                      </ErrorMessage>
+                    }
+                  ></Field>
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    as={TextField}
+                    label="PinCode"
+                    name="pincode"
+                    fullWidth
+                    variant="outlined"
+                    error={!ErrorMessage("PinCode")}
+                    helprText={
+                      <ErrorMessage>
+                        {(msg) => <span className="text-red-600">{msg}</span>}
+                      </ErrorMessage>
+                    }
+                  ></Field>
+                  <Grid>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      type="submit"
+                      color="primary"
+                    >
+                      {" "}
+                      Deliver Here
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Form>
+          </Formik>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
