a6aca9d7d4fb7758eea8d2aa03b597c452d416cc
Pratham <prathamlashkari841@gmail.com>
2024-07-25 09:55:23 +0530

    integreated accorideian

--- Online-Food-Store-Client/src/Component/Restaurant/MenuCard.jsx
+++ Online-Food-Store-Client/src/Component/Restaurant/MenuCard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
+import React from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const MenuCard = () => {
   return (
-    <div>MenuCard</div>
-  )
-}
+    <div>
+      {" "}
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion 1
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
 
-export default MenuCard
+export default MenuCard;
