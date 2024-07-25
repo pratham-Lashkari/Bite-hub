fec167dd95e305ca83a66c99a0fd226626ee8287
Pratham <prathamlashkari841@gmail.com>
2024-07-25 16:56:21 +0530

    creating profile page

---
+++ Online-Food-Store-Client/src/Component/Profile/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='lg:flex justify-between'>
+        <div className='sticky h-[80vh] lg:w-[20%]'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Profile
