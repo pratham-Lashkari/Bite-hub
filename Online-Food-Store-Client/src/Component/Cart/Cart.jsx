18ce81e88ce65dd55212c4cc280a6cb79477ecd1
Pratham <prathamlashkari841@gmail.com>
2024-07-25 10:48:07 +0530

    creting cart for user

---
+++ Online-Food-Store-Client/src/Component/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import { Divider } from "@mui/material";
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div>
+      <main className="lg:flex justify-between">
+        <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10"></section>
+        <Divider/>
+      </main>
+    </div>
+  );
+};
+
+export default Cart;
