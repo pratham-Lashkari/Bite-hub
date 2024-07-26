7ec9ee8d0e234d4d102a5ffd6b9cb44e006f74e8
Pratham <prathamlashkari841@gmail.com>
2024-07-26 14:57:35 +0530

    created setup all routes

--- Online-Food-Store-Client/src/Component/Profile/UserProfile.jsx
+++ Online-Food-Store-Client/src/Component/Profile/UserProfile.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Button } from "@mui/material";
 
 const UserProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
-}
 
-export default UserProfile
+  const handleLogOut =()=>{
+
+  }
+  return (
+    <div className="min-h-[80vh] flex flex-col justify-center items-center text-center">
+      <div className="flex flex-col items-center justify-center ">
+        <AccountCircleIcon sx={{ fontSize: "9rem" }} />
+        <h1 className="py-5 text-2xl">Pratham Lashkari</h1>
+        <p>Email : prathamlashkari@gamil.com</p>
+        <Button  variant="contained" onClick={handleLogOut} sx={{ margin: "2rem 0rem" }}>LogOut</Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
