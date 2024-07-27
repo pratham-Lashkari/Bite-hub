6069561f47648bb1d65bdd097175f38164860af1
Pratham <prathamlashkari841@gmail.com>
2024-07-27 11:59:16 +0530

    created login and register form

--- Online-Food-Store-Client/src/Component/Auth/Login.jsx
+++ Online-Food-Store-Client/src/Component/Auth/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import React from "react";
+import { Button, TextField, Typography } from "@mui/material";
+import { Formik, Form, Field } from "formik";
+import { useNavigate } from "react-router-dom";
+const initalValues = {
+  eamil: "",
+  password: "",
+};
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const handleSubmit = () => {};
+  const navigate = useNavigate();
 
-export default Login
+  return (
+    <div>
+      <Typography variant="h5" className="text-center">
+        Login
+      </Typography>
+      <Formik onSubmit={handleSubmit} initalValues={initalValues}>
+        <Form>
+          <Field
+            as={TextField}
+            label="email"
+            name="email"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          ></Field>
+          <Field
+            margin="normal"
+            as={TextField}
+            label="password"
+            name="password"
+            fullWidth
+            variant="outlined"
+          ></Field>
+          <Button
+            sx={{ mt: 2, padding: "1rem" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Login
+          </Button>
+        </Form>
+      </Formik>
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+        Don't have an account?
+        <Button size="small" onClick={() => navigate("/account/register")}>
+          register
+        </Button>
+      </Typography>
+    </div>
+  );
+};
+
+export default Login;
--- Online-Food-Store-Client/src/Component/Auth/Register.jsx
+++ Online-Food-Store-Client/src/Component/Auth/Register.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import {
+  Button,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const initialValues = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "ROLE_CUSTOMER",
+};
 
 const Register = () => {
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  const navigate = useNavigate();
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <Typography variant="h5" className="text-center">
+        Register
+      </Typography>
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        {({ values, handleChange }) => (
+          <Form>
+            <Field
+              as={TextField}
+              label="Full Name"
+              name="fullName"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            <Field
+              as={TextField}
+              label="Email"
+              name="email"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            <Field
+              as={TextField}
+              label="Password"
+              name="password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal" variant="outlined">
+              <InputLabel id="role-simple-select-label">Role</InputLabel>
+              <Field
+                as={Select}
+                labelId="role-simple-select-label"
+                id="role-simple-select"
+                name="role"
+                label="Role"
+                value={values.role}
+                onChange={handleChange}
+              >
+                <MenuItem value="ROLE_CUSTOMER">Customer</MenuItem>
+                <MenuItem value="ROLE_RESTAURANT_OWNER">
+                  Restaurant Owner
+                </MenuItem>
+              </Field>
+            </FormControl>
+            <Button
+              sx={{ mt: 2, padding: "1rem" }}
+              fullWidth
+              type="submit"
+              variant="contained"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+        If you already have an account?
+        <Button size="small" onClick={() => navigate("/account/login")}>
+          Login
+        </Button>
+      </Typography>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
