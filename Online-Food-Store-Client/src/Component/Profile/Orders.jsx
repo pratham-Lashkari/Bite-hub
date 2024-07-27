4c1d0aa5f3cb343c9d45baae577ddf54702e1fab
Pratham <prathamlashkari841@gmail.com>
2024-07-27 09:33:27 +0530

    creating order card

--- Online-Food-Store-Client/src/Component/Profile/Orders.jsx
+++ Online-Food-Store-Client/src/Component/Profile/Orders.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div className="flex items-center flex-col">
+      <h1 className="text-xl text-center py-7 font-semibold">My Orders</h1>
+      <div className="space-y-5 w-full lg:w-12">{}</div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
