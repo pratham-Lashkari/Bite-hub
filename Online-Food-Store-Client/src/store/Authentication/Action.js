ef64b492dc3546ee9fe124dcc3df124c4922852f
Pratham <prathamlashkari841@gmail.com>
2024-07-27 16:20:46 +0530

    creating reducer part

---
+++ Online-Food-Store-Client/src/store/Authentication/Action.js
@@ -0,0 +1,96 @@
+import {
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  ADD_TO_FAVORITE_REQUEST,
+  ADD_TO_FAVORITE_SUCCESS,
+  LOGOUT
+} from "./ActionTypes";
+import axios from "axios";
+import { api, API_URL } from "../../constants/api";
+
+export const registerUser = (reqData) => async (dispatch) => {
+  dispatch({ type: REGISTER_REQUEST });
+  try {
+    const { data } = await axios.post(
+      `${API_URL}/auth/signup`,
+      reqData.userData
+    );
+    if (data.jwt) localStorage.setItem("jwt", data.jwt);
+    if (data.role === "ROLE_RESTAURANT_OWNER") {
+      reqData.navigate("/admin/restaurant");
+    } else {
+      reqData.navigate("/");
+    }
+    dispatch({ type: REGISTER_SUCCESS, payload: data.jwt });
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+export const loginUser = (reqData) => async (dispatch) => {
+  dispatch({ type: LOGIN_REQUEST });
+  try {
+    const { data } = await axios.post(
+      `${API_URL}/auth/login`,
+      reqData.userData
+    );
+    if (data.jwt) localStorage.setItem("jwt", data.jwt);
+    if (data.role === "ROLE_RESTAURANT_OWNER") {
+      reqData.navigate("/admin/restaurant");
+    } else {
+      reqData.navigate("/");
+    }
+    dispatch({ type: LOGIN_SUCCESS, payload: data.jwt });
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+export const getUser = (jwt) => async (dispatch) => {
+  dispatch({ type: GET_USER_REQUEST });
+  try {
+    const { data } = await api.get("/auth/login",{
+      headers:{
+        Authorization : `Bearer ${jwt}`
+      }
+      });
+    if (data.jwt) localStorage.setItem("jwt", data.jwt);
+    if (data.role === "ROLE_RESTAURANT_OWNER") {
+      reqData.navigate("/admin/restaurant");
+    } else {
+      reqData.navigate("/");
+    }
+    dispatch({ type: GET_USER_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+export const addToFavorite = (jwt , restaurantId) => async (dispatch) => {
+  dispatch({ type: ADD_TO_FAVORITE_REQUEST });
+  try {
+    const { data } = await api.put(`/auth/restaurants/${restaurantId}/add-favorite`,{
+      headers:{
+        Authorization : `Bearer ${jwt}`
+      }
+      });
+    
+    dispatch({ type: ADD_TO_FAVORITE_SUCCESS, payload: data });
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
+
+
+export const logout = () => async (dispatch) => {
+  try {
+    dispatch({ type: LOGOUT});
+    console.log("logout success")
+  } catch (error) {
+    console.log("Error ", error);
+  }
+};
