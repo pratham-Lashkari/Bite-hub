005511b5174cc468cd2899710840092c5b4538d3
Pratham <prathamlashkari841@gmail.com>
2024-07-27 09:44:45 +0530

    created order card now creating favourites

--- Online-Food-Store-Client/src/Component/Profile/OrderCard.jsx
+++ Online-Food-Store-Client/src/Component/Profile/OrderCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Button, Card } from "@mui/material";
+import React from "react";
 
 const OrderCard = () => {
   return (
-    <div>OrderCard</div>
-  )
-}
+    <Card className="flex justify-between items-center p-5">
+      <div className="flex items-center space-x-5">
+        <img className="h-16 w-16"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*oTfm1pTXLxitHHFy.jpg"
+          alt=""
+        />
+      <div>
+        <p>Pizza</p>
+        <p>₹1499</p>
+      </div>
+      </div>
+      <div>
+        <Button className="cursor-not-allowed">completed</Button>
+      </div>
+    </Card>
+  );
+};
 
-export default OrderCard
+export default OrderCard;
--- Online-Food-Store-Client/src/Component/Profile/Orders.jsx
+++ Online-Food-Store-Client/src/Component/Profile/Orders.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import OrderCard from "./OrderCard";
 
 const Orders = () => {
   return (
     <div className="flex items-center flex-col">
       <h1 className="text-xl text-center py-7 font-semibold">My Orders</h1>
-      <div className="space-y-5 w-full lg:w-12">{}</div>
+      <div className="space-y-5 w-full lg:w-1/2">
+        {
+          [1,2,3,4,5].map((item , ind) => <OrderCard key={ind}/>)
+        }</div>
     </div>
   );
 };
