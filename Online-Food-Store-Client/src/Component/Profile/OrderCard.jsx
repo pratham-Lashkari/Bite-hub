d09effd113213732c5836d0ff7595401070ab5fb
Pratham <prathamlashkari841@gmail.com>
2024-08-03 14:19:13 +0530

    created all apis now rest work for admins

--- Online-Food-Store-Client/src/Component/Profile/OrderCard.jsx
+++ Online-Food-Store-Client/src/Component/Profile/OrderCard.jsx
@@ -1,21 +1,22 @@
 import { Button, Card } from "@mui/material";
 import React from "react";
 
-const OrderCard = () => {
+const OrderCard = ({ item }) => {
   return (
     <Card className="flex justify-between items-center p-5">
       <div className="flex items-center space-x-5">
-        <img className="h-16 w-16"
+        <img
+          className="h-16 w-16"
           src="https://miro.medium.com/v2/resize:fit:1400/0*oTfm1pTXLxitHHFy.jpg"
           alt=""
         />
-      <div>
-        <p>Pizza</p>
-        <p>₹1499</p>
+        <div>
+          <p>Pizza</p>
+          <p>₹{item.totalPrice}</p>
+        </div>
       </div>
-      </div>
       <div>
-        <Button className="cursor-not-allowed">completed</Button>
+        <Button className="cursor-not-allowed">{item.orderStatus}</Button>
       </div>
     </Card>
   );
--- Online-Food-Store-Client/src/Component/Profile/Orders.jsx
+++ Online-Food-Store-Client/src/Component/Profile/Orders.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OrderCard from "./OrderCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserOrder } from "../../store/Order/Action";
 
 const Orders = () => {
+  const order = useSelector((state) => state.order);
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    dispatch(getUserOrder(token));
+  }, []);
   return (
     <div className="flex items-center flex-col">
       <h1 className="text-xl text-center py-7 font-semibold">My Orders</h1>
       <div className="space-y-5 w-full lg:w-1/2">
-        {
-          [1,2,3].map((item , ind) => <OrderCard key={ind}/>)
-        }</div>
+        {order.orders?.map((item, ind) => (
+          <OrderCard item={item} key={ind} />
+        ))}
+      </div>
     </div>
   );
 };
