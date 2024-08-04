3d7bb285c127b1550b18bb12513af59b06728db0
Pratham <prathamlashkari841@gmail.com>
2024-08-04 16:58:43 +0530

    creating order component

--- Online-Food-Store-Client/src/Admin Component/Admin.jsx
+++ Online-Food-Store-Client/src/Admin Component/Admin.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AdminSideBar from "./AdminSideBar";
+import Dashboard from "./pages/Dashboard";
+import Orders from "./pages/Orders";
+import Menu from "./pages/Menu";
+import FoodCategory from "./pages/FoodCategory";
+import Ingredients from "./pages/Ingredients";
+import Events from "./pages/Events";
+import RestaurantDetails from "./pages/RestaurantDetails";
 
 const Admin = () => {
   return (
@@ -7,6 +15,17 @@ const Admin = () => {
       <div className="lg:flex justify-between">
         <div>
           <AdminSideBar />
+        </div>
+        <div className="lg:w-[80%]">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/category" element={<FoodCategory />} />
+            <Route path="/ingredients" element={<Ingredients />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/details" element={<RestaurantDetails />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ Online-Food-Store-Client/src/Admin Component/pages/Orders.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card, Typography } from "@mui/material";
+const Orders = () => {
+  return (
+    <div className="px-2">
+      <Card className="p-5">
+        <Typography sx={{ paddingBottom: "1rem" }} variant="h5"></Typography>
+      </Card>
+    </div>
+  );
+};
+
+export default Orders;
