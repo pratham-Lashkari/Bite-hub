2c50e4cc556f3b626f2024d38f7f541348f3913d
Pratham <prathamlashkari841@gmail.com>
2024-08-04 15:42:21 +0530

    added nav constant

---
+++ Online-Food-Store-Client/src/constants/AdminNav.jsx
@@ -0,0 +1,19 @@
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import ShopTwoIcon from "@mui/icons-material/ShopTwo";
+import CategoryIcon from "@mui/icons-material/Category";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import EventIcon from "@mui/icons-material/Event";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const adminNav = [
+  { title: "Dashboard", icon: <DashboardIcon />, path: "/" },
+  { title: "Orders", icon: <ShoppingBagIcon />, path: "/orders" },
+  { title: "Menu", icon: <ShopTwoIcon />, path: "/menu" },
+  { title: "Food Category", icon: <CategoryIcon />, path: "/category" },
+  { title: "Ingredients", icon: <FastfoodIcon />, path: "/ingredients" },
+  { title: "Events", icon: <EventIcon />, path: "/event" },
+  { title: "Details", icon: <AdminPanelSettingsIcon />, path: "/details" },
+  { title: "Logout", icon: <LogoutIcon />, path: "/logout" },
+];
