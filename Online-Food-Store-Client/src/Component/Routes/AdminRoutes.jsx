85362a07dacf0e5857164071a25c3a51202669f2
Pratham <prathamlashkari841@gmail.com>
2024-08-04 15:00:51 +0530

    set up all routes

--- Online-Food-Store-Client/src/Component/Routes/AdminRoutes.jsx
+++ Online-Food-Store-Client/src/Component/Routes/AdminRoutes.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
 const AdminRoutes = () => {
-  return <div>AdminRoutes</div>;
+  return (
+    <div>
+      <Routes>
+        <Route
+          path="/*"
+          element={false ? <CreateRestaurantForm /> : <Admin />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default AdminRoutes;
