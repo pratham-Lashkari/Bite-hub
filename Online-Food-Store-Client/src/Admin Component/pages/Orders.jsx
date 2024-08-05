fb5a6d9454595d0c35f96f68c6326f642478aa68
Pratham <prathamlashkari841@gmail.com>
2024-08-05 16:00:42 +0530

    creating order status ui for admin pannel

--- Online-Food-Store-Client/src/Admin Component/pages/Orders.jsx
+++ Online-Food-Store-Client/src/Admin Component/pages/Orders.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import { Card, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Card, FormControl, RadioGroup, Typography } from "@mui/material";
 const Orders = () => {
+  const [filterValue, setFilterValue] = useState();
   return (
     <div className="px-2">
       <Card className="p-5">
-        <Typography sx={{ paddingBottom: "1rem" }} variant="h5"></Typography>
+        <Typography sx={{ paddingBottom: "1rem" }} variant="h5">
+          Order status
+        </Typography>
+        <FormControl>
+          <RadioGroup row name="category" value={filterValue || "all"}>
+            {" "}
+          </RadioGroup>
+        </FormControl>
       </Card>
     </div>
   );
