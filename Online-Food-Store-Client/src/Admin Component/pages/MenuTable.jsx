f67c590d3656e468eef26e8c3642aaa833bb6719
Pratham <prathamlashkari841@gmail.com>
2024-08-05 17:14:22 +0530

    creating table for food_category

--- Online-Food-Store-Client/src/Admin Component/pages/MenuTable.jsx
+++ Online-Food-Store-Client/src/Admin Component/pages/MenuTable.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Card,
   CardHeader,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -11,24 +12,34 @@ import {
   TableRow,
 } from "@mui/material";
 import React from "react";
+import CreateIcon from "@mui/icons-material/Create";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const orders = [1, 2, 3, 4, 5];
 const MenuTable = () => {
   return (
     <Box>
       <Card>
-        <CardHeader title={"All Orders"} sx={{ pt: 2, alignItems: "center" }} />
+        <CardHeader
+          action={
+            <IconButton>
+              <CreateIcon />
+            </IconButton>
+          }
+          title={"All Orders"}
+          sx={{ pt: 2, alignItems: "center" }}
+        />
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 800 }} aria-label="simple table">
             <TableHead>
               <TableRow>
                 <TableCell>Id</TableCell>
                 <TableCell align="right">Image</TableCell>
-                <TableCell align="right">Customer</TableCell>
-                <TableCell align="right">Price</TableCell>
-                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Title</TableCell>
                 <TableCell align="right">Ingredients</TableCell>
-                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Price</TableCell>
+                <TableCell align="right">Availibilty</TableCell>
+                <TableCell align="right">Delete</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -37,13 +48,18 @@ const MenuTable = () => {
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                 >
-                  <TableCell>{"1"}</TableCell>
+                  <TableCell>{row}</TableCell>
                   <TableCell align="right">{"Images "}</TableCell>
                   <TableCell align="right">{"Pratahm"}</TableCell>
                   <TableCell align="right">{"3423"}</TableCell>
                   <TableCell align="right">{"Rice"}</TableCell>
                   <TableCell align="right">{"souce"}</TableCell>
-                  <TableCell align="right">{"Status"}</TableCell>
+                  <TableCell align="right">
+                    {" "}
+                    <IconButton>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
