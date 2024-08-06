a7a2521ec3d10aa58bd2a60351f708e6e8dda3a8
Pratham <prathamlashkari841@gmail.com>
2024-08-06 10:19:11 +0530

    creating restaurant detials

--- Online-Food-Store-Client/src/Admin Component/pages/RestaurantDetails.jsx
+++ Online-Food-Store-Client/src/Admin Component/pages/RestaurantDetails.jsx
@@ -1,7 +1,61 @@
 import React from "react";
-
+import { Button, Card, CardContent, CardHeader, Grid } from "@mui/material";
 const RestaurantDetails = () => {
-  return <div>Restaurant</div>;
+  const handleRestaurantStatus = () => {};
+  return (
+    <div className="lg:px-20 px-5">
+      <div className="py-5 flex justify-center items-center gap-5">
+        <h1 className="text-2xl lg:text-7xl text-center font-bold p-5">
+          Indain Fast food
+        </h1>
+        <div>
+          <Button
+            color={true ? "primary" : "error"}
+            className="py-[1rem] px-[2rem]"
+            variant="contained"
+            onClick={handleRestaurantStatus}
+            size="large"
+          >
+            {true ? "Open" : "Close"}
+          </Button>
+        </div>
+      </div>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader
+              title={<span className="text-gray-300">Restaurant</span>}
+            />
+            <CardContent>
+              <div className="space-y-4 text-gray-200">
+                <div className="flex">
+                  <p className="w-48">Owner</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Yash
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Restaurant Name</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Cusine Type</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default RestaurantDetails;
