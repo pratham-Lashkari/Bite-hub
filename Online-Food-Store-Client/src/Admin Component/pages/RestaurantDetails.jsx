ab8cc6d006dfcaee2d5924e0bf28f6d1e8cc0934
Pratham <prathamlashkari841@gmail.com>
2024-08-06 10:59:15 +0530

    added address card in restaruatn details borad

--- Online-Food-Store-Client/src/Admin Component/pages/RestaurantDetails.jsx
+++ Online-Food-Store-Client/src/Admin Component/pages/RestaurantDetails.jsx
@@ -49,6 +49,121 @@ const RestaurantDetails = () => {
                     Indian fast food
                   </p>
                 </div>
+                <div className="flex">
+                  <p className="w-48">Opening Hours</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Status</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    {true ? (
+                      <span className="px-5 py-2 rounded-full bg-green-400 text-gray-950">
+                        Open
+                      </span>
+                    ) : (
+                      <span className="px-5 py-2 rounded-full bg-red-400 text-gray-950">
+                        Closed
+                      </span>
+                    )}
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <Card>
+            <CardHeader
+              title={<span className="text-gray-300">Address</span>}
+            />
+            <CardContent>
+              <div className="space-y-4 text-gray-200">
+                <div className="flex">
+                  <p className="w-48">Country</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Inda
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">City</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indore
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Postal Code</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    452002
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Street address</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Ranjeet
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <Card>
+            <CardHeader
+              title={<span className="text-gray-300">Restaurant</span>}
+            />
+            <CardContent>
+              <div className="space-y-4 text-gray-200">
+                <div className="flex">
+                  <p className="w-48">Owner</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Yash
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Restaurant Name</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Cusine Type</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Opening Hours</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    Indian fast food
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="w-48">Status</p>
+                  <p className="text-gray-400">
+                    <span className="pr-5"> - </span>
+                    {true ? (
+                      <span className="px-5 py-2 rounded-full bg-green-400 text-gray-950">
+                        Open
+                      </span>
+                    ) : (
+                      <span className="px-5 py-2 rounded-full bg-red-400 text-gray-950">
+                        Closed
+                      </span>
+                    )}
+                  </p>
+                </div>
               </div>
             </CardContent>
           </Card>
