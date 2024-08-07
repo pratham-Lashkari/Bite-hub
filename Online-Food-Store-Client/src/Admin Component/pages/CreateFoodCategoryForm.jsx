512df70e70d47a3fbb9daf3c0265aad2089e008a
Pratham <prathamlashkari841@gmail.com>
2024-08-07 12:36:35 +0530

    added modal to create food category

---
+++ Online-Food-Store-Client/src/Admin Component/pages/CreateFoodCategoryForm.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
+const CreateFoodCategoryForm = () => {
+  const [formData, setFormData] = useState({
+    categoryName: "",
+    restaurantId: "",
+  });
+  const handleSubmit = () => {
+    const data = {
+      name: formData.categoryName,
+      restaurantId: {
+        id: 1,
+      },
+    };
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  return (
+    <div className="">
+      <div className="p-5">
+        <h1 className="text-gray-400 text-center text-xl pb-10">
+          Create Category
+        </h1>
+        <form className="space-y-5" onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            id="categoryName"
+            name="categoryName"
+            label="Category"
+            variant="outlined"
+            onChange={handleInputChange}
+            value={formData.categoryName}
+          />
+          <Button variant="contained" type="submit">
+            {" "}
+            Crate Category
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateFoodCategoryForm;
--- Online-Food-Store-Client/src/Admin Component/pages/FoodCategory.jsx
+++ Online-Food-Store-Client/src/Admin Component/pages/FoodCategory.jsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardHeader,
   IconButton,
+  Modal,
   Paper,
   Table,
   TableBody,
@@ -10,19 +11,35 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from "@mui/material";
 import React from "react";
 import CreateIcon from "@mui/icons-material/Create";
 import DeleteIcon from "@mui/icons-material/Delete";
-
+import CreateFoodCategoryForm from "./CreateFoodCategoryForm";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 const orders = [1, 2, 3, 4, 5];
 const FoodCategory = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <Box>
       <Card>
         <CardHeader
           action={
-            <IconButton>
+            <IconButton onClick={handleOpen}>
               <CreateIcon />
             </IconButton>
           }
@@ -51,6 +68,16 @@ const FoodCategory = () => {
           </Table>
         </TableContainer>
       </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <CreateFoodCategoryForm />
+        </Box>
+      </Modal>
     </Box>
   );
 };
